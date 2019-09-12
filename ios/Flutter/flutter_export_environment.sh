b0261c48a2ce31a34d52d6f8d1b54f059afafb80
Girish <pgirisha77@gmail.com>
2019-09-12 17:01:51 +0530

    project update

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\God Root\Softwares\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\God Root\Github Projects\resume_girisha"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\God Root\Softwares\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
